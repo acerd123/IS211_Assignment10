386a77a1117461b7292bfbca03c8df54b5125f31
acerd123 <Anibal.cerda86@spsmail.cuny.edu>
2024-11-09 15:23:34 -0500

    Update query_pets.py

--- query_pets.py
+++ query_pets.py
@@ -1,11 +1,11 @@
 import sqlite3
 
 def query_person_pets(person_id):
-    # Connect to the database
+    
     conn = sqlite3.connect('pets.db')
     cursor = conn.cursor()
 
-    # Query the person's data
+    
     cursor.execute("SELECT first_name, last_name, age FROM person WHERE id = ?", (person_id,))
     person = cursor.fetchone()
 
@@ -13,7 +13,7 @@ def query_person_pets(person_id):
         first_name, last_name, age = person
         print(f"{first_name} {last_name}, {age} years old")
 
-        # Query the pets of the person
+        
         cursor.execute("""
             SELECT pet.name, pet.breed, pet.age 
             FROM pet
@@ -31,7 +31,7 @@ def query_person_pets(person_id):
     else:
         print("Person not found.")
 
-    # Close the connection
+    
     conn.close()
 
 def main():
